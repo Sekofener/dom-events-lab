3f190b8f35241c93ddce20269622b601b5a60227
Serkan <serkantumur.st@gmail.com>
2024-07-11 09:47:57 -0400

    Meaningful Message Here

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
-/*-------------------------------- Constants --------------------------------*/
-const buttons = document.querySelectorAll('.button');
-const calculator = document.querySelector('#calculator');
-
-
-
+numberButtons = document.querySelectorAll(".button.number")
+const operatorButtons = document.querySelectorAll(".button.operator")
+const equalsButton = document.querySelector(".button.equals")
+const screen = document.querySelector(".display")
 /*-------------------------------- Variables --------------------------------*/
-// 
-
-
-
+let calcDisplay = ""
 /*------------------------ Cached Element References ------------------------*/
 
-
-
 /*----------------------------- Event Listeners -----------------------------*/
-buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    if (e.target.innerText === '') {
-      string = eval('string');
-      display.value = string
-    }
-    
-
-    // This log is for testing purposes to verify we're getting the correct value
-    console.log(event.target.innerText);
-    // Future logic to capture the button's value would go here...
-  });
-});
-
-
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener("click", onNumberButtonClick)
+})
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", onOperatorButtonClick)
+})
+equalsButton.addEventListener("click", onEqualsButtonClick)
 /*-------------------------------- Functions --------------------------------*/
+function onNumberButtonClick(event) {
+    const element = event.target
+    const numberString = element.innerHTML
+    const number = +numberString
+    calcDisplay += number
+    onCalculationChange()
+}
+function onOperatorButtonClick(event) {
+    const element = event.target
+    const operatorString = element.innerHTML
+    if(operatorString === "C") {
+        calcDisplay = ""
+    } else {
+        calcDisplay += operatorString
+    }
+    onCalculationChange()
+    
+}
+function onEqualsButtonClick(event) {
+    const element = event.target
+    const equalsString = element.innerHTML
+    calcDisplay = eval(calcDisplay)
+    onCalculationChange()
+}
+function onCalculationChange() {
+    screen.innerHTML = calcDisplay
+}
